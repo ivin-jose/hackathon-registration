b144db38a7942b728fc8efe853ced90a0010cbe4
ivin jose <ivinnjose@gmail.com>
2025-12-03 16:54:14 +0530

    added 500

--- main.py
+++ main.py
@@ -59,6 +59,11 @@ Subject: {subject}
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template("errors/404.html"), 404
+
+@app.errorhandler(500)
+def page_not_found(e):
+    return render_template("errors/500.html"), 500
+    
 #-------------------------------------------------------------------------------------------
 
 
